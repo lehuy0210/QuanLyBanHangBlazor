97dc899c59d14024f72c94ff1f323f0f3e4b6dbc
lehuy0210 <nhokhuypro78999@gmail.com>
2026-05-01 11:36:30 +0700

    config HttpClient and register ProductService

--- QLBH.WEB/Program.cs
+++ QLBH.WEB/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QLBH.WEB;
+using QLBH.WEB.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5003/") });
+builder.Services.AddScoped<ProductService>();
 
 await builder.Build().RunAsync();
