e8a1ed79d6156d9ee26e7d1447dfbf8ca85db389
lehuy0210 <nhokhuypro78999@gmail.com>
2026-05-01 11:52:48 +0700

    Fix BaseAddress

--- QLBH.WEB/Program.cs
+++ QLBH.WEB/Program.cs
@@ -7,7 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5003/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5003/") });
 builder.Services.AddScoped<ProductService>();
 
 await builder.Build().RunAsync();
