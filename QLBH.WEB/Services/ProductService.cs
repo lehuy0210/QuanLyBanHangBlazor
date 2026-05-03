5a139127bf0a00de87752b4aebb9065e7427e994
lehuy0210 <nhokhuypro78999@gmail.com>
2026-05-03 09:48:52 +0700

    implement CreateProductAsync, GetCategoriesAsync, GetSuppliersAsync in ProductService

--- QLBH.WEB/Services/ProductService.cs
+++ QLBH.WEB/Services/ProductService.cs
@@ -16,6 +16,22 @@ namespace QLBH.WEB.Services
             return await _http.GetFromJsonAsync<List<ProductDTO>>("api/Product");
         }
 
+        public async Task<bool> CreateProductAsync(ProductDTO sp)
+        {
+            var response = await _http.PostAsJsonAsync("api/Product", sp);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<List<CategoryDTO>> GetCategoriesAsync()
+        {
+            return await _http.GetFromJsonAsync<List<CategoryDTO>>("api/Product/categories");
+        }
+
+        public async Task<List<SupplierDTO>> GetSuppliersAsync()
+        {
+            return await _http.GetFromJsonAsync<List<SupplierDTO>>("api/Product/suppliers");
+        }
+
 
     }
 }
