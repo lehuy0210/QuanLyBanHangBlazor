57814888104726d4823c4d3de995f45b6ab65d29
lehuy0210 <nhokhuypro78999@gmail.com>
2026-05-06 15:01:00 +0700

    add AddBlazoredLocalStorage, AddAuthorizationCore and AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>

--- QLBH.WEB/Program.cs
+++ QLBH.WEB/Program.cs
@@ -1,6 +1,9 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QLBH.WEB;
+using QLBH.WEB.Providers;
 using QLBH.WEB.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,6 +11,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5003/") });
+builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<ProductService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
